90af368a55d32db8ffb0fbec08b3d8e9f4f14c64
陈冠华 <chengh@tuya.com>
2020-03-18 18:09:23 +0800

    feat：小修改

--- client/src/pages/index/index.jsx
+++ client/src/pages/index/index.jsx
@@ -1,39 +1,40 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Image, Input, Icon, Button } from '@tarojs/components'
 import './index.less'
 
 export default class Index extends Component {
-
   config = {
     navigationBarTitleText: '首页'
   }
 
-  componentWillMount () { }
+  componentWillMount() {}
 
-  componentDidMount () { }
+  componentDidMount() {}
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  render () {
+  render() {
     return (
-    <View class="header">
-      <Icon></Icon>
-      <Text>登录</Text>
-    </View>
+      <View>
+        <View class='header'>
+          <Icon></Icon>
+          <Text>登录</Text>
+        </View>
 
-    <View class="main">
-      <Image src="../../res/1.png" mode="aspectFit"></Image>
-      <Input type="text" placeholder="手机号/邮箱"></Input>
-      <Image src="../../res/2.png" mode="aspectFit"></Image>
-      <Input type="password" placeholder="请输入密码"></Input>
-    </View>
+        <View class='main'>
+          <Image src='../../res/1.png' mode='aspectFit'></Image>
+          <Input type='text' placeholder='手机号/邮箱'></Input>
+          <Image src='../../res/2.png' mode='aspectFit'></Image>
+          <Input type='password' placeholder='请输入密码'></Input>
+        </View>
 
-    <Button>登录</Button>
-    <Button>注册</Button>
+        <Button>登录</Button>
+        <Button>注册</Button>
+      </View>
     )
   }
 }
